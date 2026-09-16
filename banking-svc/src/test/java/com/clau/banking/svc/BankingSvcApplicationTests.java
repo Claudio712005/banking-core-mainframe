4c2bd753088b113f7a46e11e1a332faeedeb33e5
Claudio712005 <clausilvaaraujo11@gmail.com>
2026-09-16 20:20:32 -0300

    feat: adicionando estrutura base do svc

---
+++ banking-svc/src/test/java/com/clau/banking/svc/BankingSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clau.banking.svc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingSvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
